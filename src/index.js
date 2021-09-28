f7a5afc9662009c6e9e422c782c235a061d40b28
Arsen7878 <81430202+Arsen7878@users.noreply.github.com>
2021-09-28 12:28:55 +0000

    Deploying to gh-pages from @ Arsen7878/kirill-form@fc7e8efd387e6926d5d90784b2b1767c6d4ae8b5 🚀

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import app from './js/validator';
-import form from './js/form';
-import button from './js/button-back';
-import calendar from './js/calendar';
-import price from './js/price';
-import priceInteraction from './js/price-interaction';
